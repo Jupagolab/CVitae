17f4d09b6a22bacbb25e82548a68b0d7fadb33d0
Jupagolab <114892574+Jupagolab@users.noreply.github.com>
2024-07-23 17:19:15 -0400

    imagen

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
   return (
-    <div className='mx-auto text-center'>
-      <header className='bg-black p-4 text-white font-bold'>
-        <h1 className='text-2xl'>Curriculum Vitae</h1>
+    <div className="mx-auto text-center">
+      <header className="bg-black p-4 text-white font-bold">
+        <h1 className="text-3xl">Curriculum Vitae</h1>
       </header>
       <main>
-        <div className='flex justify-center items-center'>
-          <img src="./images/juan.jpg" alt="Foto de Juan González" />
+        <div className="flex justify-center items-center flex-col m-4 md:m-7 xl:m-9 gap-4">
+          <img className="rounded-full w-24 transform transition-transform duration-300 hover:scale-125" src="./images/juan.jpg" alt="Foto de Juan González" />
+          <h2 className="text-xl font-bold">Juan Pablo Gonzalez Labarca</h2>
         </div>
-        <h2 className='font-bold'>Juan Pablo Gonzalez Labarca</h2>
-
-        p
       </main>
     </div>
-  )
+  );
 }
 
 export default App;
