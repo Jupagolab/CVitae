b36e205d6bb3689119e79598f5ce5c3b8562a24c
Jupagolab <114892574+Jupagolab@users.noreply.github.com>
2024-07-23 15:02:37 -0400

    Inicializacion del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className='mx-auto text-center'>
+      <header className='bg-black p-4 text-white font-bold'>
+        <h1 className='text-2xl'>Curriculum Vitae</h1>
+      </header>
+      <main>
+        <div className='flex justify-center items-center'>
+          <img src="./images/juan.jpg" alt="Foto de Juan González" />
+        </div>
+        <h2 className='font-bold'>Juan Pablo Gonzalez Labarca</h2>
+
+        p
+      </main>
+    </div>
+  )
+}
+
+export default App;
