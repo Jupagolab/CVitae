cb9333d279bc03aeef865b5175e3a55ea70ccaef
Jupagolab <114892574+Jupagolab@users.noreply.github.com>
2024-07-24 16:06:58 -0400

    Correccion de Errores

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import AnimatePage from "./components/Animation/AnimatePage";
 
 function App() {
   return (
@@ -10,15 +11,27 @@ function App() {
       <Router>
         <Header />
         <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<h1>About </h1>} />
-            <Route path="/skills" element={<h1>Skills</h1>} />
-            <Route path="/experience" element={<h1>Experience</h1>} />
-            <Route path="/education" element={<h1>Education</h1>} />
-            <Route path="/testimonials" element={<h1>Testimonials </h1>} />
-            <Route path="/contact" element={<h1>Contact</h1>} />
-          </Routes>
+          <AnimatePage>
+            <div className="flex justify-center items-center flex-col m-4 md:m-7 xl:m-9 gap-4">
+              <img
+                className="rounded-full w-24 transform transition-transform duration-300 hover:scale-125"
+                src="./images/juan.jpg"
+                alt="Foto de Juan González"
+              />
+              <h2 className="text-xl lg:text-2xl font-bold">
+                Juan Pablo Gonzalez Labarca
+              </h2>
+            </div>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<h1>About </h1>} />
+              <Route path="/skills" element={<h1>Skills</h1>} />
+              <Route path="/experience" element={<h1>Experience</h1>} />
+              <Route path="/education" element={<h1>Education</h1>} />
+              <Route path="/testimonials" element={<h1>Testimonials </h1>} />
+              <Route path="/contact" element={<h1>Contact</h1>} />
+            </Routes>
+          </AnimatePage>
         </div>
         <Footer />
       </Router>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,14 @@ const Header = () => {
   return (
     <>
       <header className="bg-custom-gradient p-4 text-white font-bold">
-        <div className="space-x-4">
-        <Link to={"/"}><h1 className="text-3xl lg:mb-4">Curriculum Vitae</h1></Link>
-          <Link to="/about">Sobre Mí</Link>
-          <Link to="/projects">Proyectos</Link>
-          <Link to="/skills">Habilidades</Link>
-          <Link to="/experience">Experiencia</Link>
-          <Link to="/education">Educación</Link>
-          <Link to="/testimonials">Testimonios</Link>
-          <Link to="/contact">Contacto</Link>
+        <Link to={"/"}><h1 className="text-3xl lg: lg:mb-4">Curriculum Vitae</h1></Link>
+        <div className="space-x-4 lg:space-x-8">
+          <Link className='border-b-2 border-b-transparent hover:text-blue-200 hover:border-b-white py-2' to="/">Sobre Mí</Link>
+          <Link className='border-b-2 border-b-transparent hover:text-blue-200 hover:border-b-white py-2' to="/skills">Habilidades</Link>
+          <Link className='border-b-2 border-b-transparent hover:text-blue-200 hover:border-b-white py-2' to="/experience">Experiencia</Link>
+          <Link className='border-b-2 border-b-transparent hover:text-blue-200 hover:border-b-white py-2' to="/education">Educación</Link>
+          <Link className='border-b-2 border-b-transparent hover:text-blue-200 hover:border-b-white py-2' to="/testimonials">Testimonios</Link>
+          <Link className='border-b-2 border-b-transparent hover:text-blue-200 hover:border-b-white py-2' to="/contact">Contacto</Link>
         </div>
       </header>
     </>
